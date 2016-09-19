c4aa39707a28e564222ace36481f9f3f301f90e7
anybalrocha <anybalrocha@hotmail.com>
2016-09-18 21:05:42 -0300

    Inclusão de projeto.

---
+++ README.md
@@ -0,0 +1,2 @@
+# ASP.NET MVC + Entity Framework
+Projeto destinado aos desenvolvedores ASP.NET que queiram conhecer o MVC, com a utilização do Entity Framework para conectar e executar comandos de banco de dados, bem como manter a estrutura de banco de dados atualizada de acordo com a estrutura de código, com aplicação do Code First e Migrations.
---
+++ WebApplication1/Controllers/PessoaController.cs
@@ -0,0 +1,77 @@
+﻿using Service.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PessoaController : Controller
+    {
+        //
+        // GET: /Pessoa/
+        public ActionResult Index()
+        {
+            ViewBag.Mensagem = "Minha primeira View";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Pessoa model)
+        {
+            ModelState.Remove("Codigo");
+
+            if (ModelState.IsValid)
+            {
+                PessoaService srv = new PessoaService();
+                srv.Salvar(model);
+
+                return View("List", srv.Listar());
+            }
+            else
+                return View(model);
+        }
+
+        public ActionResult List()
+        {
+            PessoaService srv = new PessoaService();
+            return View(srv.Listar());
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var srv = new PessoaService();
+            return View("Create", srv.Obter(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Pessoa model)
+        {
+            if (ModelState.IsValid)
+            {
+                PessoaService srv = new PessoaService();
+                srv.Salvar(model);
+
+                return View("List", srv.Listar());
+            }
+            else
+                return View("Create", model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            PessoaService srv = new PessoaService();
+            srv.Deletar(id);
+
+            return View("List", srv.Listar());
+        }
+	}
+}
